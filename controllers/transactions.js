e46043fc64cdc79ed4403345a0aa024f27bf924e
albertohuerta <albertohuerta22@gmail.com>
2022-07-02 17:03:49 +0100

    added controller file for routes and verified get, post, delete api calls

---
+++ controllers/transactions.js
@@ -0,0 +1,21 @@
+// @desc  Get all transactions
+// @route /api/v1/transactions
+// @access Public
+
+exports.getTransactions = (req, res, next) => {
+  res.send('GET transactions');
+};
+// @desc  Add transactions
+// @route POST /api/v1/transactions
+// @access Public
+
+exports.addTransaction = (req, res, next) => {
+  res.send('POST transactions');
+};
+// @desc  Delete transaction
+// @route DELETE /api/v1/transactions/:id
+// @access Public
+
+exports.deleteTransaction = (req, res, next) => {
+  res.send('Delete transaction');
+};
