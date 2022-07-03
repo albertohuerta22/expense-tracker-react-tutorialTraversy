8a509814ab082c5c1110a406434ad1a5cad18008
albertohuerta <albertohuerta22@gmail.com>
2022-07-03 14:49:43 +0100

    added additional erorr handling validition messages

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 const Transactions = require('../models/Transactions');
 
 // @desc  Get all transactions
@@ -28,7 +29,32 @@ exports.getTransactions = async (req, res, next) => {
 // @access Public
 
 exports.addTransaction = async (req, res, next) => {
-  res.send('POST transactions');
+  try {
+    const { text, amount } = req.body;
+    const transaction = await Transactions.create(req.body);
+
+    res.status(201);
+    return res.json({
+      success: true,
+      data: transaction,
+    });
+  } catch (err) {
+    if (err.name === 'ValidationError') {
+      const messages = Object.values(err.errors).map((val) => val.message);
+
+      res.status(400); // the client didnt send what it was suppose to
+      return res.json({
+        success: false,
+        error: messages,
+      });
+    } else {
+      res.status(500);
+      return res.json({
+        success: false,
+        error: 'Server Error',
+      });
+    }
+  }
 };
 // @desc  Delete transaction
 // @route DELETE /api/v1/transactions/:id
